a68847c8c910c27a01efed9777cef34c2759ef12
chisun-joung <csjoung@gmail.com>
2017-02-21 16:14:54 +0900

    square_init

--- square.py
+++ square.py
@@ -1,7 +1,12 @@
+square = []
+square_size = 0
+
 def in_proc():
+    global square_size
     value = input("Enter of square num : ")
     if value.isdigit():
         value = int(value)
+        square_size = value
     else:
         print("Invalid value")
     return value
@@ -15,14 +20,17 @@ def square_odd():
 def square_even():
     pass
 
+def square_init():
+    global square
+    square = [ i for i in range(square_size**2)]
+
 def main():
     loop = True
     while loop:
         value = in_proc()
-        print(value)
         if value == 0 :
             loop = False
-
+        square_init()
 
 
 if __name__ == "__main__":
