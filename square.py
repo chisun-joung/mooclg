501f5f7a60c12a57488e4f3f288878fd8b3a1aaa
chisun-joung <csjoung@gmail.com>
2017-02-22 08:59:48 +0900

    in_proc() out_proc()

--- square.py
+++ square.py
@@ -1,18 +1,21 @@
 square = []
 square_size = 0
+isvalid_input = False
 
 def in_proc():
-    global square_size
+    global square_size, isvalid_input
+    isvalid_input = False
     value = input("Enter of square num : ")
     if value.isdigit():
         value = int(value)
-        square_size = value
-    else:
-        print("Invalid value")
+        if value > 2 and (value%2 == 1 or value%4 == 0):
+            isvalid_input = True
+            square_size = value
     return value
 
 def out_proc():
-    pass
+    for i in range(square_size):
+        print(square[i*square_size:i*square_size + square_size])
 
 def square_odd():
     pass
@@ -22,7 +25,7 @@ def square_even():
 
 def square_init():
     global square
-    square = [ i for i in range(square_size**2)]
+    square = [i for i in range(square_size**2)]
 
 def main():
     loop = True
@@ -30,8 +33,13 @@ def main():
         value = in_proc()
         if value == 0 :
             loop = False
+            continue
+        if not isvalid_input:
+            print("Invalid value")
+            continue
         square_init()
-
+        out_proc()
+    print("===== End =====")
 
 if __name__ == "__main__":
     main()
