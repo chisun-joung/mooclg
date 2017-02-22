453ae62f3d9df00112652074feddec5cb2712d95
chisun-joung <csjoung@gmail.com>
2017-02-23 00:08:54 +0900

    joseph.py

---
+++ joseph.py
@@ -0,0 +1,25 @@
+def circular_push_back():
+    pass
+
+def circular_erase():
+    pass
+
+def circular_show():
+    pass
+
+def in_proc():
+
+
+def out_proc():
+    pass
+
+def extraction():
+    pass
+
+def main():
+    loop = True
+    while loop:
+        in_proc()
+
+if __name__ == "__main__":
+    main()
