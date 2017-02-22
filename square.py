db27097939592a396f6fc326b2c8e7be74e89a73
chisun-joung <csjoung@gmail.com>
2017-02-22 13:40:19 +0900

    finish formatting output

--- square.py
+++ square.py
@@ -1,27 +1,33 @@
 square = []
 square_size = 0
 isvalid_input = False
-
+isodd_square = False
 def in_proc():
-    global square_size, isvalid_input
-    isvalid_input = False
+    global square_size, isvalid_input, isodd_square
+    isvalid_input, isodd_square = False , False
     value = input("Enter of square num : ")
     if value.isdigit():
         value = int(value)
         if value > 2 and (value%2 == 1 or value%4 == 0):
             isvalid_input = True
             square_size = value
+            if value%2 == 1:
+                isodd_square = True
     return value
 
 def out_proc():
+    span = len(str(max(square)))
     for i in range(square_size):
-        print(square[i*square_size:i*square_size + square_size])
+        ret = square[i*square_size:i*square_size + square_size]
+        for j in ret:
+            print("{0:>{1}} ".format(j,span),end = '')
+        print()
 
 def square_odd():
-    pass
+    print("2n+1 Solving")
 
 def square_even():
-    pass
+    print("4n Solving")
 
 def square_init():
     global square
@@ -38,6 +44,10 @@ def main():
             print("Invalid value")
             continue
         square_init()
+        if isodd_square:
+            square_odd()
+        else:
+            square_even()
         out_proc()
     print("===== End =====")
 
